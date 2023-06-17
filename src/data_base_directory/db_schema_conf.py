0c5c3560cb85a77f0e13718e469e5be43389e7e8
Asenim <pasha_me1998@mail.ru>
2023-06-17 04:56:14 +0400

    Создана корректная схема базы данных, добавлены методы удаления и добавления данных в БД

--- src/data_base_directory/db_schema_conf.py
+++ src/data_base_directory/db_schema_conf.py
@@ -2,8 +2,8 @@
 from sqlalchemy import Integer, VARCHAR, \
     ForeignKey, Column, create_engine
 # Импорт класса для наследования наших классов-таблиц и
-# Session - служит для работы с БД
-from sqlalchemy.orm import declarative_base, Session
+# Класс Session - служит для работы с БД из других файлов
+from sqlalchemy.orm import declarative_base, relationship, Session
 
 # Импорт констант для подключения к базе данных
 from config_env import SQL_DRIVER, DB_HOST, \
@@ -15,7 +15,10 @@ from config_env import SQL_DRIVER, DB_HOST, \
 # ############################### #
 
 engine = create_engine(f'{SQL_DRIVER}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
-session = Session(engine)
+# Объект session будет создаваться  #
+# каждый раз при обращении к БД     #
+# в методах - сервисах приложения   #
+# Пример: session = Session(bind=engine) #
 Base = declarative_base()
 
 # ############################### #
@@ -26,14 +29,33 @@ Base = declarative_base()
 class Players(Base):
     __tablename__ = "Players"
 
-    ID = Column(Integer(), primary_key=True)
+    ID = Column(Integer(), primary_key=True, autoincrement=True)
     Name = Column(VARCHAR(255), nullable=False, index=True)
+    # matches = relationship(
+    #     "Matches",
+    #     back_populates="players",
+    #     cascade="all, delete",
+    #     passive_deletes=True
+    # )
 
 
 class Matches(Base):
     __tablename__ = "Matches"
 
-    ID = Column(Integer(), primary_key=True)
-    Player1 = Column(Integer(), ForeignKey('Players.ID'), nullable=False)
-    Player2 = Column(Integer(), ForeignKey('Players.ID'), nullable=False)
-    Winner = Column(Integer(), ForeignKey('Players.ID'), nullable=False)
+    ID = Column(Integer(), primary_key=True, autoincrement=True)
+    Player1 = Column(Integer(), ForeignKey('Players.ID', ondelete="CASCADE"), nullable=False)
+    Player2 = Column(Integer(), ForeignKey('Players.ID', ondelete="CASCADE"), nullable=False)
+    Winner = Column(Integer(), ForeignKey('Players.ID', ondelete="CASCADE"), nullable=False)
+
+    players1 = relationship(
+        "Players",
+        foreign_keys=[Player1],
+    )
+    players2 = relationship(
+        "Players",
+        foreign_keys=[Player2],
+    )
+    winners = relationship(
+        "Players",
+        foreign_keys=[Winner],
+    )
--- src/finished_matches_persistence_service.py
+++ src/finished_matches_persistence_service.py
@@ -1,50 +1,112 @@
-from src.data_base_directory.db_schema_conf \
-    import *
+from src.data_base_directory.db_schema_conf import *
+import logging
 
 
 # players
-def insert_players():
-    """
-    Функция будет добавлять игроков в таблицу players
-    :return:
-    """
-    p1 = Players(
-        Name='Ziya'
-    )
-    print(p1)
-    print(p1.Name)
-    session.add(p1)
-    print(session.new)
-    session.commit()
+class InteractionTablePlayers:
+    @staticmethod
+    def insert_one_player(name):
+        """
+        Функция добавляет игроков в таблицу players
+        :return:
+        """
+        session = Session(bind=engine)
+        try:
+            __player = Players(
+                Name=name
+            )
+            session.add(__player)
+            print(session.new)
+            session.commit()
+            print("Data added to db")
+        except ConnectionError:
+            print("Failed to connect to database")
+        finally:
+            session.close()
+            print("Session closed!")
+
+    @staticmethod
+    def delete_one_player(name=None, id=None):
+        """
+        Функция будет удалять игроков из таблицы players
+        :return:
+        """
+        session = Session(bind=engine)
+        try:
+            if id is not None:
+                __player = session.query(Players).filter(Players.ID == id).one()
+                session.delete(__player)
+                session.commit()
+                print(f"Data {__player} deleted!")
+            else:
+                __player = session.query(Players).filter(Players.Name == name).one()
+                session.delete(__player)
+                session.commit()
+                print(f"Data {__player} deleted!")
+
+        except ConnectionError:
+            print("Failed to connect to database")
+        finally:
+            session.close()
+            print("Session closed!")
 
 
-def delete_players():
-    """
-    Функция будет удалять игроков из таблицы players
-    :return:
-    """
+class InteractionTableMatches:
+    @staticmethod
+    def insert_matches(p1_id, p2_id, p_win):
+        """
+        Функция добавляет результаты игры в таблицу matches
+        В эту таблицу могут быть добавлены только игроки из
+        таблицы Players.
+        :param p1_id: ID player1 (Column name: Player1)
+        :param p2_id: ID player2 (Column name: Player2)
+        :param p_win: ID player winner (Column name: Winner)
+        """
+        session = Session(bind=engine)
+        try:
+            __insert_match = Matches(
+                Player1=p1_id,
+                Player2=p2_id,
+                Winner=p_win,
+            )
+            session.add(__insert_match)
+            print(session.new)
+            session.commit()
+            print("Data added to db")
+        except ConnectionError:
+            print("Failed to connect to database")
+        finally:
+            session.close()
+            print("Session closed!")
 
-    player = session.query(Players).filter(Players.ID == '1').one()
-    session.delete(player)
-    session.commit()
+    @staticmethod
+    def delete_matches(id_match):
+        """
+        Функция удаляет данные из таблицы matches
+        по ID.
+        :param id_match: ID матча который будем удалять
+        """
+        session = Session(bind=engine)
+        try:
+            __player = session.query(Players).filter(Matches.ID == id_match).one()
+            session.delete(__player)
+            session.commit()
+            print(f"Data {__player} deleted!")
+
+        except ConnectionError:
+            print("Failed to connect to database")
+        finally:
+            session.close()
+            print("Session closed!")
 
 
-# matches
-def insert_matches():
-    """
-    Функция добавляет результаты игры в таблицу matches
-    :return:
-    """
-    pass
+play = InteractionTablePlayers()
+# play.insert_one_player('Alfob')
+# play.insert_one_player('Ziya')
+# play.insert_one_player('Kubla')
+# play.insert_one_player('Sergey')
+# play.delete_one_player(id=3)
+# play.delete_one_player(name='Ziya')
 
-
-def delete_matches():
-    """
-    Функция удаляет данные из таблицы matches
-    :return:
-    """
-    pass
-
-
-# insert_players()
-delete_players()
+matches = InteractionTableMatches()
+# matches.insert_matches(3, 4, 4)
